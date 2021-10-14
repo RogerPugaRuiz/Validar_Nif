88394ed00bef97147c3914f693c52d750f2060e4
RogerPugaRuiz <rogerpugaruiz@gmail.com>
2021-10-14 18:00:41 +0200

    first commit

---
+++ src/main/java/com/mycompany/Person_with_exceptions/Exceptions/PropertyNotInitializedException.java
@@ -0,0 +1,13 @@
+package com.mycompany.Person_with_exceptions.Exceptions;
+
+/**
+ * PropertyNotInitializedException.java
+ * PropertyNotInitializedException class
+ * @author Jose
+ * @version 
+ */
+ public class PropertyNotInitializedException extends Exception {
+	public PropertyNotInitializedException(String msg) {
+		super(msg);						
+	}
+}
